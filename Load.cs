88291ac6cc1b74753faeeb04c1cb8c5f43b9c748
laweacosmica <elian-alejandro2000@hotmail.com>
2017-01-17 03:04:40 -0300

    Add files via upload

---
+++ Load.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace AutoLvLUp
+{
+    internal class Load
+    {
+        internal static void Init()
+        {
+            try
+            {
+                AutoLvlup.Autolvlup.Init();
+                Lantern.PickLantern.Init();
+                Pings.PingDetector.Init();
+                Skin.SkinChanger.Init();
+                Warding.WardsHelper.Init();
+                Warding.AntiStealth.Init();
+                DPSCalculator.DPS.Init();
+            }
+            catch (Exception ex)
+            {
+                Logger.Send("Error At KappaUtility.Brain.Utility.Misc.Load.Init", ex, Logger.LogLevel.Error);
+            }
+        }
+    }
+}
